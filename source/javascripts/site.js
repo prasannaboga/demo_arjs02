58c71a10ba1444c162a4fd8be239dc77c11799b1
prasanna.boga <prasannaboga@gmail.com>
2018-04-20 18:18:17 +0530

    Some changes

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,26 @@
-// This is where it all goes :)
+$(document).ready(function () {
+  try {
+    window.selectedCamera = '';
+    navigator.mediaDevices.enumerateDevices()
+      .then(function (result) {
+        var videoDevices = [];
+        $(result).each(function (i, videoDevice) {
+          if (i === 0) {
+            window.selectedCamera = videoDevice;
+          }
+          if (videoDevice.kind === 'videoinput') {
+            videoDevices.push(videoDevice);
+            $('#listCameras').append($("<option>", {value: videoDevice.deviceId, html: videoDevice.label}));
+          }
+        });
+        //console.log(videoDevices);
+      })
+      .catch(function (error) {
+        alert(error.message);
+        console.error(error);
+      });
+  } catch (err) {
+    alert(err.message);
+    console.error(err);
+  }
+});
